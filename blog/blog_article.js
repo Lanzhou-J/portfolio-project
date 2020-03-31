1305ad486cf1bca2b8db769efbda595032ed9ebf
Lanzhou Jiang <lzjiang99@gmail.com>
2020-03-31 22:03:34 +1100

    Finished formatting blog

--- blog/blog_article.js
+++ blog/blog_article.js
@@ -1,23 +1,28 @@
   function displayArticle1() {
-  document.getElementById("a_title").innerHTML = "Article1"
-  document.getElementById("a_paragraph").innerHTML = "Article1 content1"
+  document.getElementById("a_title").innerHTML = ""
+  document.getElementById("a_paragraph").innerHTML = "  <div class='bolg_all_paragraph' id='lake_paragraph'><div class='wrapper itemawrapper'><div class='item ia'><p class='date'>30 . 03 . 2020</p><p class='straitline'>|</p><p class='weather'>Sunny</p></div></div><div class='wrapper itembwrapper'><div class='item ib'><img src='../docs/myart/IMG_0004.JPG' alt='cats' class='art1'></div></div><div class='wrapper itemcwrapper'><div class='item ic'><img src='../docs/myart/IMG_0124.JPG' alt='Rabbits' class='art2'></div></div><div class='wrapper itemdwrapper'><div class='item id'><p class='textd'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis voluptatibus quaerat nostrum inventore? Cumque exercitationem voluptas atque ipsam nobis possimus impedit? Perspiciatis, id cum asperiores magni dolore voluptatum et eaque! Aut, aperiam in deserunt vero fuga quam iure facilis laborum.</p></div></div></div>"
 }
 
 function displayArticle2() {
-  document.getElementById("a_title").innerHTML = "Article2"
-  document.getElementById("a_paragraph").innerHTML = "Article2 content1"
+  document.getElementById("a_title").innerHTML = ""
+  document.getElementById("a_paragraph").innerHTML = "<div class='bolg_all_paragraph' id='procreate_paragraph'><div class='wrapper itemawrapper'><div class='item ia'><p class='date'>05 . 03 . 2020</p><p class='straitline'>|</p><p class='weather'>Cloudy</p></div></div><div class='wrapper itembwrapper'><div class='item ib'><img src='../docs/plants/bluebeans.png' alt='plants' class='plants'></div></div><div class='wrapper itemcwrapper'><div class='item ic'><p class='textc'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, id cum asperiores magni dolore voluptatum et eaque! Aut, aperiam in deserunt vero fuga quam iure facilis laborum. Perferendis voluptatibus quaerat nostrum inventore. Perferendis voluptatibus quaerat nostrum inventore? Cumque exercitationem voluptas atque ipsam nobis possimus impedit? Perspiciatis, id cum asperiores magni dolore voluptatum et eaque! </p></div></div><div class='wrapper itemdwrapper'><div class='item id'><p class='textd'>Cumque exercitationem voluptas atque ipsam nobis possimus impedit? Perspiciatis, id cum asperiores magni dolore voluptatum et eaque! Aut, aperiam in deserunt vero fuga quam iure facilis laborum. Perferendis voluptatibus quaerat nostrum inventore.<br><br>Perferendis voluptatibus quaerat nostrum inventore. Cumque exercitationem voluptas atque ipsam nobis possimus impedit? </p></div></div></div>"
 }
 function displayArticle3() {
-  document.getElementById("a_title").innerHTML = "Article3"
-  document.getElementById("a_paragraph").innerHTML = "Article3 content1"
+  document.getElementById("a_title").innerHTML = ""
+  document.getElementById("a_paragraph").innerHTML = " <div class='bolg_all_paragraph' id='procreate_paragraph'><div class='wrapper itemawrapper'><div class='item ia'><p class='date'>23 . 02 . 2020</p><p class='straitline'>|</p><p class='weather'>Cloudy</p></div></div><div class='wrapper itembwrapper'><div class='item ib'><img src='../docs/myart/IMG_0120.PNG' alt='portrait' class='portrait'></div></div><div class='wrapper itemcwrapper'><div class='item ic'><img src='../docs/myart/IMG_0142.PNG' alt='Rabbits' class='cat'></div></div><div class='wrapper itemdwrapper'><div class='item id'><p class='textd'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis voluptatibus quaerat nostrum inventore. <br><br>Cumque exercitationem voluptas atque ipsam nobis possimus impedit? Perspiciatis, id cum asperiores magni dolore voluptatum et eaque! Aut, aperiam in deserunt vero fuga quam iure facilis laborum. Perferendis voluptatibus quaerat nostrum inventore.</p></div></div></div>"
 }
 
 function displayArticle4() {
-  document.getElementById("a_title").innerHTML = "Article4"
-  document.getElementById("a_paragraph").innerHTML = "Article4 content1"
+  document.getElementById("a_title").innerHTML = ""
+  document.getElementById("a_paragraph").innerHTML = "<div class='bolg_all_paragraph' id='procreate_paragraph'><div class='wrapper itemawrapper'><div class='item ia'><p class='date'>20 . 02 . 2020</p><p class='straitline'>|</p><p class='weather'>Windy</p></div></div><div class='wrapper itembwrapper'><div class='item ib'><img src='../docs/undraw_book_reading_kx9s.svg' alt='plants' class='plants'></div></div><div class='wrapper itemcwrapper'><div class='item ic'><p class='textc'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, id cum asperiores magni dolore voluptatum et eaque! Aut, aperiam in deserunt vero fuga quam iure facilis laborum.<br><br> Perferendis voluptatibus quaerat nostrum inventore. Perferendis voluptatibus quaerat nostrum inventore? Cumque exercitationem voluptas atque ipsam nobis possimus impedit? </p></div></div><div class='wrapper itemdwrapper'><div class='item id'><p class='textd'>Aut, aperiam in deserunt vero fuga quam iure facilis laborum. Perferendis voluptatibus quaerat nostrum inventore. <br><br>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, id cum asperiores magni dolore voluptatum et eaque! Aut, aperiam in deserunt vero fuga quam iure facilis laborum. Aut, aperiam in deserunt vero fuga quam iure facilis laborum. Perferendis voluptatibus quaerat nostrum inventore.</p></div></div></div>"
 }
 
 function displayArticle5() {
-  document.getElementById("a_title").innerHTML = "Article5"
-  document.getElementById("a_paragraph").innerHTML = "Article5 content1"
+  document.getElementById("a_title").innerHTML = ""
+  document.getElementById("a_paragraph").innerHTML = " <div class='bolg_all_paragraph' id='procreate_paragraph'><div class='wrapper itemawrapper'><div class='item ia'><p class='date'>15 . 02 . 2020</p><p class='straitline'>|</p><p class='weather'>Sunny</p></div></div><div class='wrapper itembwrapper'><div class='item ib'><img src='../docs/brekfast/IMG_3519.JPG' alt='breakfast1' class='arvo_sandwitch'></div></div><div class='wrapper itemcwrapper'><div class='item ic'><img src='../docs/brekfast/IMG_3826.jpg' alt='Breakfast2' class='cat'></div></div><div class='wrapper itemdwrapper'><div class='item id'><p class='textd'>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br><br> Perferendis voluptatibus quaerat nostrum inventore. Cumque exercitationem voluptas atque ipsam nobis possimus impedit? Perspiciatis, id cum asperiores magni dolore voluptatum et eaque! Aut, aperiam in deserunt vero fuga quam iure facilis laborum. Perferendis voluptatibus quaerat nostrum inventore.</p></div></div></div>"
+}
+
+function displaywelcome() {
+  document.getElementById("a_title").innerHTML = " "
+  document.getElementById("a_paragraph").innerHTML = " <div class='welcome_paragraph'><p class='welcome'>Welcome to my blog!</p><img src='../docs/undraw_making_art_759c.svg' alt='making art'><p class='select_left'>⬅&nbsp;&nbsp; Read more...</p></div>"
 }
